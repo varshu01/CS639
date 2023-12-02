75617ac9bacf24be408a0a238f730d381531bbd1
varshu01 <51811929+varshu01@users.noreply.github.com>
2023-12-01 22:12:16 -0500

    Adding RetroFit assignment

---
+++ android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
@@ -0,0 +1,35 @@
+
+package com.example.android.marsphotos.overview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.android.marsphotos.databinding.FragmentOverviewBinding
+
+
+class OverviewFragment : Fragment() {
+
+    private val viewModel: OverviewViewModel by viewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentOverviewBinding.inflate(inflater)
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.lifecycleOwner = this
+
+        // Giving the binding access to the OverviewViewModel
+        binding.viewModel = viewModel
+
+        // Sets the adapter of the photosGrid RecyclerView
+        binding.photosGrid.adapter = PhotoGridAdapter()
+
+        return binding.root
+    }
+}
